11226e51adff36695b0976455ebc4f314e84da51
dyoung223 <davisy30@gmail.com>
2023-12-03 08:49:04 +0000

    adding working version of part 4 for assignment 4

--- asst4/module.cpp
+++ asst4/module.cpp
@@ -409,6 +409,176 @@ torch::Tensor myFlashAttention(torch::Tensor QTensor, torch::Tensor KTensor, tor
     std::vector<float> lnew = formatTensor(LnewTensor);
 
     // -------- YOUR CODE HERE  -------- //
+    int zeroRow = 0;
+
+    for (int b = 0; b < B; b++){
+        //loop over heads
+        for (int h = 0; h < H; h++){
+            //for (int i = 0; i < N ; i++){
+	    
+            std::fill(l.begin(), l.end(), 0);
+            for (int col_block = 0; col_block < N; col_block = col_block + Bc){  
+            /*for(int col = col_block; col < std::min(col_block+BC,N); col++){
+                for(int j = 0; j < d; j++){
+                    float val_k = fourDimRead(Q, b, h, col, j, H, N, d); 
+                float val_v = fourDimRead(V, b, h, j, col, H, N, d);
+                twoDimWrite(Kj, col, j, val_k);
+                twoDimWrite(Vj, col, j, val_v);
+                }
+            }*/
+                
+                //printf("Beginning new col_block %i\n", col_block);
+                for (int row_block = 0; row_block < N; row_block = row_block + Br){
+                    std::fill(Sij.begin(), Sij.end(), 0);
+                    //printf("Beginning new row_block %i\n", row_block);
+                    std::fill(lij.begin(), lij.end(), 0);
+                    //load li, Oi
+                    std::fill(li.begin(), li.end(), 0);
+                    std::fill(Oi.begin(), Oi.end(), 0);
+                    for (int row = row_block; row < std::min(row_block + Br, N); row++){
+                        int true_row = row - row_block;
+                        li[true_row] = l[row];
+                        for(int j = 0; j < d; j++){
+                            float val_oi = fourDimRead(O, b, h, row, j, H, N, d);
+                            twoDimWrite(Oi, true_row, j, d, val_oi);
+                        }
+                    }
+                    for (int row = row_block; row < std::min(row_block + Br, N); row++){
+                        int true_row = row - row_block;
+                        //printf("Beginning new row %i\n", row);
+                        float val_li = li[true_row];
+                        float val_lij = lij[true_row];
+                        for (int col = col_block; col < std::min(col_block + Bc, N); col++){
+                            int true_col = col - col_block;
+                            //printf("Beginning new col %i\n", col);
+                            float val_qkt = twoDimRead(Sij, true_row, true_col, Br);
+                            //printf("Read in Sij for row %i, col %i\n", row, col);
+                            for(int j = 0; j < d; j++){
+                                float val_q = fourDimRead(Q, b, h, row, j, H, N, d);
+                                float val_kt = fourDimRead(K, b, h, col, j, H, N, d);
+                                val_qkt += val_q * val_kt;
+                            }
+                            //printf("Calculated qkt\n");
+                            twoDimWrite(Sij, true_row, true_col, Br, val_qkt);
+                            float exp_val_qkt = exp(val_qkt);
+                            twoDimWrite(Pij, true_row, true_col, Br, exp_val_qkt);
+                            val_lij += exp_val_qkt;
+                            lij[true_row] = val_lij;
+
+                        }
+                    //printf("Completed rowsum of exp\n");
+                        float my_lnew = val_li + lij[true_row];
+                        lnew[true_row] = my_lnew;
+                        for(int j = 0; j < d; j++){
+                            float O_i_to_read = twoDimRead(Oi, true_row, j, d); //This could be wrong here!!!					   
+                            float output_sum = 0.f;
+                            for (int col = col_block; col < std::min(col_block + Bc, N); col++){
+                                int true_col = col - col_block;
+                                float exp_val = twoDimRead(Pij, true_row, true_col, Br);
+                                float v_val = fourDimRead(V, b, h, col, j, H, N, d);
+                                //float v_val = twoDimRead(Vj, col, j, d);
+                                output_sum += exp_val * v_val;
+                            }
+                            float O_to_write = (val_li*O_i_to_read + output_sum)/lnew[true_row];
+                            //printf("Writing to output row: %i, j: %i \n", row, j);
+                            twoDimWrite(Oi, true_row, j, d, O_to_write);
+                            fourDimWrite(O, b, h, row, j, H, N, d, O_to_write);
+                        }
+                        l[row] = lnew[true_row];
+                    }
+                }
+            }	
+            //printf("Bc: %i, Br: %i, B: %i, H: %i, N: %i, d: %i\n", Bc, Br, B, H, N, d);
+            //printf("SIZES: O: %i, Q: %i, K: %i, V: %i, Sij: %i, Pij: %i, Kj: %i, Vj: %i, Qi: %i Oi: %i, l: %i, pv: %i, li: %i, lij: %i, lnew: %i\n", O.size(), Q.size(), K.size(), V.size(), Sij.size(), Pij.size(), Kj.size(), Vj.size(), Qi.size(), Oi.size(), l.size(), PV.size(), li.size(), lij.size(), lnew.size());
+	    }
+    }
+	    /*
+	    for (int col_block = 0; col_block < N; col_block = col_block + Bc){  
+		for (int col = col_block; col < std::min(col_block + Bc, N); col++){
+		    for(int j = 0; j < d; j++){
+
+		   	float val_k = fourDimRead(Q, b, h, col, j, H, N, d); 
+			float val_v = fourDimRead(V, b, h, j, col, H, N, d);
+			twoDimWrite(Kj, col, j, val_k);
+			twoDimWrite(Vj, col, j, val_v);
+			
+		        }
+
+		    for (int row_block = 0; row_block < N; row_block = row_block + Br){
+		        for (int row = row_block; row < std::min(row_block + Br, N); row++){
+			    //twoDimWrite(li, zeroRow, row, 0.f);
+		            for(int j= 0; j < d; j++){
+			        float val_q = fourDimRead(Q, b, h, row, j, H, N, d);
+			        twoDimWrite(Qi, row, j, val_q);
+			        twoDimWrite(Oi, row, j, 0.f);
+		  	    }
+		        }
+	   	    }
+    		    //Compute Si_j	
+		    for (int row_block = 0; row_block < N; row_block = row_block + Br){
+		        for (int row = row_block; row < std::min(row_block + Br, N); row++){
+                    	    float val_qkt = 0.0;
+		            for(int j= 0; j < d; j++){
+			    // SHOULD THIS BE ACCESSED USING N?
+			        float val_q = twoDimRead(Qi, row, j, N);
+			        float val_k = twoDimRead(Kj, col, j, N)
+			        val_qkt += val_1 * val_kt;
+			    }
+			    twoDimWrite(Sij, row, col, N, val_qkt);
+			    float P_ij = exp(val_qkt);
+			    twoDimWrite(Pij, row, col, N, val_qkt);
+			    float row_sum = twoDimRead(lij, zeroRow, row, N); //row or col?
+			    row_sum += P_ij;
+			    twoDimWrite(lij, zeroRow, row, N, row_sum);  //row or col?
+				
+		        }
+	   	    }
+		}
+		
+	    }
+
+	    
+	
+	    for (int qkt1 = 0; qkt1 < N; qkt1++){
+	        float sum_exp_val_qkt = 0.0;
+	        for (int qkt2 = 0; qkt2 < N; qkt2++){
+	 	    float val_qkt = twoDimRead(QK_t, qkt1, qkt2, N);
+		    float exp_val_qkt = exp(val_qkt);
+		    sum_exp_val_qkt += exp_val_qkt;
+	        }
+	        for (int qkt2 = 0; qkt2 < N; qkt2++){
+		    float val_qkt = twoDimRead(QK_t, qkt1, qkt2, N);
+		    float exp_val_qkt = exp(val_qkt);
+		    float val_softmax = exp_val_qkt / sum_exp_val_qkt;
+		    //printf("exp_val_qkt = %f ", exp_val_qkt);      
+		    //printf("sum_exp_val_qkt = %f ", sum_exp_val_qkt);      
+		    //printf("val_softmax = %f ", val_softmax);    
+		    twoDimWrite(QK_t, qkt1, qkt2, N, val_softmax);
+	        }
+	    }
+	
+	    for (int o1_b = 0; o1_b < N; o1_b = o1_b + L){
+                for (int o2_b = 0; o2_b < d; o2_b = o2_b + L){  
+		    for( int j_b = 0; j_b < N; j_b = j_b + L){  
+		        for (int o1 = o1_b; o1 < std::min(o1_b + L, N); o1++){
+			    for (int o2 = o2_b; o2 < std::min(o2_b + L, d); o2++){
+			        float val_o = fourDimRead(O, b, h, o1, o2, H, N, d);
+			        for(int j = j_b; j < std::min(j_b + L,  N); j++){
+				    float val_softmax = twoDimRead(QK_t, o1, j, N);
+				    float val_v = fourDimRead(V, b, h, j, o2, H, N, d);
+				    val_o += val_softmax * val_v;                        
+			        }
+			//printf("val_qkt = %f", val_qkt);
+			        fourDimWrite(O, b, h, o1, o2, H, N, d, val_o);	
+			    }
+		        }
+		    }
+	        }
+ 	    }           
+*/	
+
+
+
 
     // DO NOT EDIT THIS RETURN STATEMENT //
     // It formats your C++ Vector O back into a Tensor of Shape (B, H, N, d) and returns it //
